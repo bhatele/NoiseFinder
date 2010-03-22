95c667c695db7a41a4ceafc8aa325fd0248bba9c
Abhinav Bhatele <bhatele@jaguar12.ccs.ornl.gov>
2010-03-22 02:57:53 -0400

    first check in

---
+++ NoiseFinder.c
@@ -0,0 +1,203 @@
+/** \file NoiseFinder.c
+ *  Author: Abhinav S Bhatele (bhatelele@illinois.edu)
+ *  Previous contributors: Authors: Eric Bohm, Chee Wai L
+ *
+ *  A simple benchmark to quantify computational noise
+ */
+
+#include <mpi.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+#define UNIT_WORK_FACTOR	10000
+#define TAG			255
+#define NUMBINS			200
+
+inline int doUnitWork(int);
+
+int main(int argc, char **argv) {
+  int myrank, size;
+  int i, j, pos;
+  int iterations;
+  int worksize;
+
+  /* Statistics data */
+  double sum = 0.0;
+  double sum_of_squares = 0.0;
+  double min = 0.0;
+  double max = 0.0;
+  double globalmin = 0.0;
+  double globalmax = 0.0;
+  double mean = 0.0;
+  double variance = 0.0;
+  double stddev = 0.0;
+  int hist[NUMBINS+1];
+  int histSum[NUMBINS+1];
+
+  for(i=0; i<NUMBINS+1; i++) {
+    hist[i] = 0;
+    histSum[i] = 0;
+  }
+
+  // used to prevent the compiler from optimizing the delay loop away.
+  double dummydata = 0.0;
+
+  /* Timing related variables */
+  double prevtime, currtime, intime = 0.0;
+  double overhead = 0.0;
+  double prevouterttime, outtime, outmin, outmax;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  int errlen;
+  char usage[100+MPI_MAX_ERROR_STRING]="Usage: NoiseFinder NumIterations Grainsize\n";
+
+  if(argc<3) {
+    printf(usage);
+    fflush(stdout);
+    MPI_Abort(MPI_COMM_WORLD,10);
+  }
+  iterations = atoi(argv[1]);
+  worksize = atoi(argv[2]);
+
+  /* Walltime overheads */
+  prevtime = MPI_Wtime();
+  for (i=0; i<1000; i++) {
+    MPI_Wtime();
+  }
+  overhead = (MPI_Wtime() - prevtime)/1000;
+
+
+  int donework = 0;
+  struct valpair {
+    double val;
+    int rank;
+  } oneminp, onemaxp, *minpair, *maxpair, maxdevpair, *outminpair, *outmaxpair;
+
+  minpair = (struct valpair *) malloc (iterations * sizeof(struct valpair));
+  maxpair = (struct valpair *) malloc (iterations * sizeof(struct valpair));
+  outminpair = (struct valpair *) malloc (iterations * sizeof(struct valpair));
+  outmaxpair = (struct valpair *) malloc (iterations * sizeof(struct valpair));
+
+  prevouterttime = MPI_Wtime();
+  outmin = prevouterttime*1000;
+  globalmin = prevouterttime*1000;
+  outmax = 0.0;
+  globalmax = 0.0;
+
+  for(j=0; j<iterations; j++) {
+    prevtime = MPI_Wtime();
+    min = prevtime*1000.0;
+    max = 0.0;
+
+    for(i=0; i<iterations; i++) {
+      donework = doUnitWork(worksize);
+      currtime = MPI_Wtime();
+      intime =  currtime - prevtime;
+      sum += intime;
+      sum_of_squares += intime * intime;
+      min = (min > intime) ? intime : min;
+      max = (max < intime) ? intime : max;
+      globalmin = (globalmin > intime) ? intime : globalmin;
+      globalmax = (globalmax < intime) ? intime : globalmax;
+      pos = (int) (intime / 0.000001); // size of each bin = 1 us
+      if(pos < NUMBINS)
+	hist[pos]++;
+      else
+	hist[NUMBINS]++;
+      prevtime = MPI_Wtime();
+    }
+    outtime = currtime-prevouterttime;
+    prevouterttime = currtime;
+    outmin = (outmin > outtime) ? outtime : outmin;
+    outmax = (outmax < outtime) ? outtime : outmax;
+
+    oneminp.val = min;
+    oneminp.rank = myrank;
+    onemaxp.val = max;
+    onemaxp.rank = myrank;
+    MPI_Allreduce(&oneminp, &minpair[j], 1, MPI_DOUBLE_INT, MPI_MINLOC, MPI_COMM_WORLD);
+    MPI_Allreduce(&onemaxp, &maxpair[j], 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+
+    oneminp.val = outmin;
+    oneminp.rank = myrank;
+    onemaxp.val = outmax;
+    onemaxp.rank = myrank;
+    MPI_Allreduce(&oneminp, &outminpair[j], 1, MPI_DOUBLE_INT, MPI_MINLOC, MPI_COMM_WORLD);
+    MPI_Allreduce(&onemaxp, &outmaxpair[j], 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+
+    /*  Barrier is pretty redundant
+    MPI_Barrier(MPI_COMM_WORLD);
+    */
+  }
+
+  /* Compute Statistics */
+  mean = sum/(iterations*iterations);
+  stddev = sqrt((sum_of_squares - 2*mean*sum + mean*mean*iterations*iterations) / 
+		(iterations*iterations - 1));
+  onemaxp.val=stddev;
+  MPI_Allreduce(&onemaxp, &maxdevpair, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+  
+  MPI_Reduce(&hist, &histSum, NUMBINS+1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  
+  /* Print Summary */
+  char name[30];
+  sprintf(name, "noise_pe%d", myrank);
+  FILE *outf = fopen(name, "w");
+  fprintf(outf, "Rank %d : Mean = %f Min = %f Max = %f StdDev = %f\n", myrank, mean, globalmin, globalmax, stddev);
+  fflush(NULL);
+  fclose(outf);
+
+  if(myrank==0)
+  { /** so the compiler can't wish away your work */
+     printf("donework %d\n", donework);
+  }
+  fflush(NULL);
+
+  MPI_Barrier(MPI_COMM_WORLD);    
+  if(myrank==0) {
+    outf = fopen(name, "a");
+    fprintf(outf, "Rank %d : Num Steps = %d\n", myrank, iterations);
+    fprintf(outf, "Rank %d : MPI_Wtime overhead per step = %f\n", myrank, overhead);
+    fprintf(outf, "Rank %d : worksize %d\n", myrank, worksize);
+    for(i=0;i<iterations; i++) {
+      fprintf(outf, "Iter[%d] max %f on rank %d min %f on rank %d\n", i, maxpair[i].val, maxpair[i].rank, minpair[i].val, minpair[i].rank);
+      fprintf(outf, "Iter[%d] outer max %f on rank %d min %f on rank %d\n", i, outmaxpair[i].val, outmaxpair[i].rank, outminpair[i].val, outminpair[i].rank);
+    }
+    fprintf(outf, "Rank[%d] had max dev %f \n", maxdevpair.rank, maxdevpair.val);
+
+    for(i=0; i<NUMBINS+1; i++)
+      fprintf(outf, "Hist %d %d\n", i, histSum[i]);
+
+    fclose(outf);
+  }
+
+  /*
+  mean = sum/iterations*iterations;
+  stddev = sqrt((sum_of_squares - 2*mean*sum + mean*mean*iterations*iterations) / 
+		(iterations*iterations - 1));
+
+  printf("Rank %d : Average steptime = %f\n", myrank, mean);
+  printf("Rank %d : donework %d\n",myrank, donework);
+  */
+
+  fflush(stdout);
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Finalize();
+}
+
+inline int doUnitWork(int repeat) {
+  double dummydata = 0.0;
+  int i, repeatcount;
+
+  for (repeatcount=0; repeatcount<repeat; repeatcount++) {
+    for (i=0; i<UNIT_WORK_FACTOR; i++) {
+      dummydata += i*repeatcount;
+    }
+  }
+
+  return (((int)dummydata)*repeat);
+}
